77aa6f58e67aa930b9e41ee36b31ba728a1e0509
Elroi <itsxelliex@gmail.com>
2025-11-15 22:44:18 -0500

    making additions

--- web/web/server.py
+++ web/web/server.py
@@ -1,6 +1,9 @@
 from flask import flask, request, jsonify
 app = flask(__name__)
 
+@app.route("/")
+def home():
+  return app.send_static_file
 @app.route("/rate", methods=["POST"])
 def rate():
   data  = request.get_json()
