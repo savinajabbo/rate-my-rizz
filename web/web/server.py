4bc257eb99ff81e6a9647ec84a13546c8da3cee6
Elroi <itsxelliex@gmail.com>
2025-11-15 23:15:17 -0500

    changes

--- web/web/server.py
+++ web/web/server.py
@@ -1,5 +1,28 @@
-from flask import flask, request, jsonify
-app = flask(__name__)
+from flask import Flask, Response, request, jsonify
+import cv2
+app = Flask(__name__)
+
+camera = cv2.VideoCapture(0)
+
+def generate_flames():
+  while True:
+    success, frame = camera.read()
+    if not success:
+      break
+    else:
+      _, buffer = cv2.imencode('.jpg', frame)
+    frame = buffer.tobytes()
+    
+    yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route("/video")
+def video():
+  return Response(generate_flames(), 
+                  mimetype='multipart/x-mixed-replace; boundary=frame')
+  
+  @app.route("index.html")
+  def index():
+    return open("index.html").read()
 
 @app.route("/")
 def home():
